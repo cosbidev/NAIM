87487cb164ab7f243a00d53a98d23ba9986b334c
cm.caruso <12844500+cmcaruso@users.noreply.github.com>
2025-01-18 10:14:57 +0100

    Update specific_models.py

--- CMC_utils/save_load/specific_models.py
+++ CMC_utils/save_load/specific_models.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-from kan import KAN
 from xgboost import XGBClassifier
 from pytorch_tabnet.tab_model import TabNetClassifier
 from typing import Union
@@ -9,7 +8,7 @@ from CMC_utils.save_load import save_table
 from CMC_utils.paths import add_extension, get_extension, remove_extension
 
 
-__all__ = ["save_tabnet_model", "load_tabnet_model", "save_xgboost_model", "load_xgboost_model", "save_kan_model", "load_kan_model"]
+__all__ = ["save_tabnet_model", "load_tabnet_model", "save_xgboost_model", "load_xgboost_model"]
 
 
 def save_xgboost_model(model: XGBClassifier, model_name: str, model_dir: str, extension: str = None, model_params: Union[dict, DictConfig, pd.Series, pd.DataFrame] = None, **kwargs) -> None:
@@ -105,25 +104,6 @@ def load_tabnet_model(path: str) -> TabNetClassifier:
     """
     model = TabNetClassifier()
     model.load_model(path)
-    return model
-
-
-def save_kan_model(model: KAN, model_name: str, model_dir: str, extension: str = None, model_params: Union[dict, DictConfig, pd.Series, pd.DataFrame] = None, **kwargs):
-
-    model.save_ckpt(model_name, model_dir)
-
-    if model_params is not None:
-        model_params_df = pd.DataFrame(pd.Series(model_params).reset_index()).rename({"index": "params", 0: "values"}, axis=1)
-        model_name = remove_extension(model_name)
-
-        save_table(model_params_df, model_name, model_dir, extension="csv")
-
-
-def load_kan_model(path: str, model: KAN) -> KAN:
-    model_name = os.path.basename(path)
-    model_dir = os.path.dirname(path)
-
-    model.load_ckpt(model_name, model_dir)
     return model
 
 
